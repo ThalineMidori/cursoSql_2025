5a5aeb0a1f2118bb134b6df8563de80c91599968
Thaline Martins <midorisuuta09@gmail.com>
2025-09-26 14:20:08 -0400

    acrescentando as modificações

--- Aulas/15_win_function.sql
+++ Aulas/15_win_function.sql
@@ -1,1 +1,49 @@
 -- Quanto tempo leva para um cliente voltar?
+
+--clientes do ano de 2025, ordernando por id e por data
+WITH tb_cliente_dia AS (    
+    SELECT DISTINCT
+            IdCliente,
+            substr(DtCriacao, 1, 10) AS dtDia
+
+    FROM transacoes
+
+    WHERE substr(DtCriacao, 1, 4) = '2025'
+
+    GROUP BY IdCliente, dtDia
+
+),
+
+-- colocando o lag para depois calcular a diferença dos dias
+tb_lag AS(    
+    SELECT  *,
+            lag(dtDia) OVER (PARTITION BY IdCliente ORDER BY dtDia) AS lagDia
+
+    FROM tb_cliente_dia
+),
+
+-- saber a diferença de dias de presença para cada cliente
+tb_diff_dias AS (
+
+    SELECT  *,
+        julianday(dtDia) - julianday(lagDia) AS dtDiff
+
+    FROM tb_lag
+),
+
+-- para saber média de dias por cliente 
+tb_avg_cliente AS (
+    
+    SELECT  IdCliente,
+            avg(dtDiff) AS avgDia
+
+    FROM tb_diff_dias
+
+    GROUP BY IdCliente
+
+)
+
+-- para saber a média de dias gerais
+SELECT  avg(avgDia)
+
+FROM tb_avg_cliente
